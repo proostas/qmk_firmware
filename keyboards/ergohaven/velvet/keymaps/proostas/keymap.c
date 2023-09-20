3fc6ad5529c0da5a7dbd953c347b290acdaa4d76
Stas Prokofyev <stas@prokofyev.me>
2023-09-20 06:29:56 +0300

    Fix Qwerty layer

--- keyboards/ergohaven/velvet/keymaps/proostas/keymap.c
+++ keyboards/ergohaven/velvet/keymaps/proostas/keymap.c
@@ -40,6 +40,7 @@ enum layer_names {
 #define MOS_ENT LT(MOS, KC_ENT)
 #define FUN_TAB LT(FUN, KC_TAB)
 #define NUM_SPC LT(NUM, KC_SPC)
+#define QWN_BSP LT(QWN, KC_BSPC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -61,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     GUI_A,   ALT_S,   SFT_D,   CTL_F,   KC_G,                           KC_H,    CTL_J,   SFT_K,   ALT_L,   GUI_SC,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                      XXXXXXX, XXXXXXX, CON_BSP, MOS_ENT,      FUN_TAB, NUM_SPC, XXXXXXX, XXXXXXX
+                      XXXXXXX, XXXXXXX, QWN_BSP, MOS_ENT,      FUN_TAB, NUM_SPC, XXXXXXX, XXXXXXX
   ),
 
   [_QWERTY_NAV] = LAYOUT(
